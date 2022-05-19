2b4e794b0e6bc3590685ec677cc2f53b10481646
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-05-19 00:50:04 -0500

    comments

--- Recursion/main.java
+++ Recursion/main.java
@@ -1,6 +1,11 @@
 package Recursion;
 
 public class main {
+    /*
+     * -Recursion is the calling of function several times
+     * -used to break down big problem into smaller one
+     */
+
     public static void main(String[] args) {
         recursivemethod(5);
     }
@@ -9,7 +14,8 @@ public class main {
         if (n < 1) {
             System.out.println("n is less than 1");
         } else {
-            recursivemethod(n - 1);
+            recursivemethod(n - 1); // it recursivelly calls it self serveral times till it reach less than 1 which
+                                    // provided a termination to the recursion
             System.out.println(n);
         }
     }
