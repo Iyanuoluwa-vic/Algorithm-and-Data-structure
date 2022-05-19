9d30b8fbdef7d7ae4d2d90a456ee614bd4353069
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-05-19 09:20:22 -0500

    caluclate the power of numbers using recursion

---
+++ Recursions/RecursionInterview2.java
@@ -0,0 +1,30 @@
+package Recursions;
+
+/**
+ * How to calculate power of a number using recursion?
+ * x^n = x * x^(n-1)
+ * 
+ * 
+ */
+
+public class RecursionInterview2 {
+    public static void main(String[] args) {
+
+        System.out.println(recursionPower(2, 3));
+
+    }
+
+    public static int recursionPower(int n, int power) {
+        if (n < 0) { // unintentional case
+            return -1;
+        }
+
+        if (power == 0) { // base case
+            return 1;
+        }
+
+        return n * recursionPower(n, power - 1); // caluclate the power of numbers using recursion
+
+    }
+
+}
