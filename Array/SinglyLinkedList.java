9f01ac768698aa32837fa10bcbb47d8802758ad2
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-05-20 09:47:45 -0500

    create an array

---
+++ Array/SinglyLinkedList.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class SinglyLinkedList {
+    int arr[]=null;
+
+
+    public SinglyLinkedList(int size){// constructor than create an array
+        arr =new int [size];
+        for(int i=0; i <arr.length;i++){
+               arr[i]=Integer.MIN_VALUE;    //Hold lowest value integer can have
+        }
+    }
+
+
+    public static void 
+}
