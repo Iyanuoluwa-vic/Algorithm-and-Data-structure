6382ff1a368a049fff2e2354b7872876950760dd
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-05-20 10:17:55 -0500

    create an array

---
+++ Array/Main.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyDimesionalArray sin = new SinglyDimesionalArray(5);
+        sin.arrayInsertion(0, 5);
+
+    }
+}
