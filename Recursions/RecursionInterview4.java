a0843379733c63707e08c5af9d1dee37b38c61a1
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-05-19 19:06:39 -0500

    convert a number from Decimal to Binary using recursion

---
+++ Recursions/RecursionInterview4.java
@@ -0,0 +1,27 @@
+package Recursions;
+/*
+      How to convert a number from Decimal to Binary using recursion
+          
+         
+*/
+
+public class RecursionInterview4 {
+    public static void main(String[] args) {
+
+        System.out.println(recursionDecimalToBinary(48));
+
+    }
+
+    public static int recursionDecimalToBinary(int n) {
+        if (n < 0) { // unintentional case
+            return -1;
+        }
+
+        if (n == 0) { // base case
+            return n;
+        }
+
+        return n % 2 + 10 * recursionDecimalToBinary(n / 2); // convert a number from decimaal to binary using recursion
+
+    }
+}
