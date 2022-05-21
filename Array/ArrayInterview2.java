c6d87f2b780ee155c4b1bc7bae3f9c4b33209129
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-05-20 19:39:36 -0500

    sum of two number to get a specific target number

--- Array/ArrayInterview2.java
+++ Array/ArrayInterview2.java
@@ -7,7 +7,7 @@ public class ArrayInterview2 {
         twoSum(intArray, 20);
     }
 
-    // sum of two number to get a specific target number
+    // sum of two number to get a specific target number.
     public static void twoSum(int intArray[], int target) {
         for (int i = 0; i < intArray.length; i++) {
             for (int j = i + 1; j < intArray.length; j++) {
