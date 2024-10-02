3f851422b9a2bb237a706d5dd11105b86dc0e837
VictorAdeyeri <adeyeriiyanu@gmail.com>
2024-10-02 13:36:20 -0400

    Creating an Array

---
+++ Array/MissingNumber/ArrayCreation.java
@@ -0,0 +1,26 @@
+package Array.MissingNumber;
+import java.util.Arrays;
+public class ArrayCreation {
+    public static void main(String[] args) {
+
+        // cREATE AN ARRAY
+        int[] array1 = new int[3];
+
+
+        // Time Complexity of this three is 0(n)
+        array1[0]=3;
+        array1[1]=33;
+        array1[2]=34;
+
+        System.out.println(Arrays.toString(array1));
+
+        String[] names ={"James", "Victor", "Sarah"};
+
+        System.out.println(Arrays.toString(names));
+
+
+
+
+    }
+    
+}
